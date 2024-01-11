1976187b6e1ce360a5f2860846b0f27ceca9374b
Alyson Morais <alysonmorais35@hotmail.com>
2024-01-11 16:56:55 -0300

    Placeholder scenary

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _smoothTime;
+
+    private Vector3 _currentVelocity = Vector3.zero;
+    private Vector3 _offset;
+
+    private void Awake()
+    {
+        _offset = transform.position - _target.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = _target.position + _offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _currentVelocity, _smoothTime);
+    }
+}
